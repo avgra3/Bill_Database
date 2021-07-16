42fe74dca5e5d29399bc5e7aa292f83f18087bed
avgra3 <antonygradillas@gmail.com>
2021-07-16 10:13:43 -0700

    Updated comments

--- billing/bills/admin.py
+++ billing/bills/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Carriers, Products, Bills, BillsPaid
 
-# Register your models here.
+# Registers models here so we can view them on the admin site.
 admin.site.register(Carriers)
 admin.site.register(Products)
 admin.site.register(Bills)
