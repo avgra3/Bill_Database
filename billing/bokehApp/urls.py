c3654cee6317052efdf813cfb9e92b9f34754dd5
avgra3 <antonygradillas@gmail.com>
2021-07-16 20:44:51 -0700

    Multiple bokeh plots tested

--- billing/bokehApp/urls.py
+++ billing/bokehApp/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('starter/', views.starter, name='starter'),
     path('leaning/', views.leaning, name='leaning'),
+    path('combo/', views.combo, name='combo'),
+    path('programming/', views.programming, name='programming'),
+    path('multi_plot/', views.multi_plot, name='multi_plot'),
 ]
