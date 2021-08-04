d0cdba02d0284419994989f6c8612548f0cf5ab8
avgra3 <antonygradillas@gmail.com>
2021-08-04 09:38:20 -0700

    Updated formatting of trigger

--- SQL_scripts/update_monthly_breakdown_table.sql
+++ SQL_scripts/update_monthly_breakdown_table.sql
@@ -10,10 +10,10 @@ CREATE TRIGGER update_billbreakdown_tbl
 		INSERT INTO bills_monthlybreakdown
 			(myPaid, totalPaid)
 			-- VALUES -- (NEW.paidDate, NEW.totalPaid)
-			SELECT SUM(totalPaid), MONTH(paidDate)
+			SELECT SUM(totalPaid), DATE_FORMAT(paidDate, '%m - %Y')
 			FROM bills_billspaid
-			GROUP BY MONTH(paidDate);
-			
+			GROUP BY DATE_FORMAT(paidDate, '%m - %Y')
+			ORDER BY paidDate;
 	END//
 	
 DELIMITER ;
