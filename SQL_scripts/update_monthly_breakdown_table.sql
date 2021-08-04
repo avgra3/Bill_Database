e335b964d3855bcb10864bc3e5a9a8208ab76458
avgra3 <antonygradillas@gmail.com>
2021-08-04 08:45:31 -0700

    added updated trigger for monthly breakdown

--- SQL_scripts/update_monthly_breakdown_table.sql
+++ SQL_scripts/update_monthly_breakdown_table.sql
@@ -4,12 +4,15 @@ DROP TRIGGER IF EXISTS update_billbreakdown_tbl;
 
 DELIMITER //
 CREATE TRIGGER update_billbreakdown_tbl
-	AFTER INSERT ON bills_billspaid
+	AFTER INSERT ON bills_billpaid
 	FOR EACH ROW
 	BEGIN
 		INSERT INTO bills_monthlybreakdown
 			(myPaid, totalPaid)
-			VALUES(NEW.paidDate, NEW.totalPaid);
+			-- VALUES -- (NEW.paidDate, NEW.totalPaid)
+			SELECT SUM(totalPaid), MONTH(paidDate)
+			FROM bills_billspaid
+			GROUP BY MONTH(paidDate);
 			
 	END//
 	
