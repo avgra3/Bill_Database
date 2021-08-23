38ca2432c302d0586251e177625c1ea66d35fb57
avgra3 <antonygradillas@gmail.com>
2021-08-23 10:57:09 -0700

    Added filter for bill paid on admin site

--- billing/bills/admin.py
+++ billing/bills/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from .models import Carrier, Product, Bill, BillPaid, MonthlyBreakdown
 
+# Create a filter for paid bills
+class ProfileBillPaid(admin.ModelAdmin):
+    list_filter = ("paidBool", "paidDate")
+
 # Registers models here so we can view them on the admin site.
 admin.site.register(Carrier)
 admin.site.register(Product)
 admin.site.register(Bill)
-admin.site.register(BillPaid)
+admin.site.register(BillPaid, ProfileBillPaid)
 admin.site.register(MonthlyBreakdown)
